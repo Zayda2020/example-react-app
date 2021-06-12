b856d7fb9c4fc640813a4c9fd78c8006b73f0859
Bhavik Sheth <bhaviksheth20@gmail.com>
2021-06-12 14:14:55 +0100

    add goals list

---
+++ src/components/GoalsList/index.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import GoalItem from "../GoalItem"
+
+const GoalsList = (props) => {
+  return (
+    <div className="goals-list">
+      {/* <GoalItem title={props.goals[0].title} />
+      <GoalItem title={props.goals[1].title} />
+      <GoalItem title={props.goals[2].title} />
+      <GoalItem title={props.goals[3].title} />
+      <GoalItem title={props.goals[4].title} /> */}
+
+      {props.goals.map((goal) => (
+        <GoalItem key={goal.id} title={goal.title} />
+      ))}
+
+    </div>
+  )
+}
+
+export default GoalsList
